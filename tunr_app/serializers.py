f894d29b35138a4760ff188e1b284bd56cb815af
nedmorgan <nmorgan1987@gmail.com>
2019-04-11 12:14:27 -0400

    added routes for api

---
+++ tunr_app/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import Artist, Song
+
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Song
+        fields: ('id', 'title', 'song', 'preview_url', 'artist')
+
+
+class ArtistSerializer(serializers.ModelSerializer):
+    songs = SongSerializer(many=True, read_only=True)
+
+    class Meta:
+        model: Artist
+        fields: ('id', 'name', 'photo_url', 'nationality', 'songs')
